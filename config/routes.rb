303429ddce09b45655279d7c0ff9d32c386cf9a3
David JunWei <junwei@focus100.tw>
2019-10-14 14:20:53 +0000

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
     resources :groups
   end
   root 'groups#index'
+  root "topics#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
